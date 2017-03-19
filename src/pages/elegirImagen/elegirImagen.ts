2f7333877b34c4a2ac35d3efae14db0ae2688ad1
Ryan Wadsworth <ryanrain@gmail.com>
2017-03-19 09:37:40 -0600

    add audio

--- src/pages/elegirImagen/elegirImagen.ts
+++ src/pages/elegirImagen/elegirImagen.ts
@@ -1,8 +1,10 @@
-import { Component, ViewChildren, AfterViewInit, QueryList, ElementRef } from '@angular/core';
+import { Component, ViewChildren, ViewChild, AfterViewInit, QueryList, ElementRef } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/merge';
+import 'rxjs/add/operator/throttleTime';
+
 import { NavController } from 'ionic-angular';
 
 import { AlfabetoCastillano } from '../../alfabetos/alfabeto.castillano';
@@ -15,18 +17,16 @@ import { LetraModel } from '../../models/letra.model';
   <button class="volver" (click)="volver()"><ion-icon name="home"></ion-icon></button>
 
   <ion-content padding [class.arriba]="imagenArriba">
-    <div id="vertical-container">
-      <div id="letra-principal">
-        <div>
-          <span *ngIf="palabraAhora == ''">{{letraAhora}}</span>
-          {{palabraAhora}}
-        </div>
+    <div #letraAhoraRef id="letra-principal">
+      <div>
+        <span *ngIf="palabraAhora == ''">{{letraAhora}}</span>
+        {{palabraAhora}}
       </div>
-      <div id="imagenes-letra">
-        <img #opcion1 src="assets/imagenes/{{opciones[0].palabra}}.png" [id]="opciones[0].letra" [class]="opciones[0].palabra">
-        <img #opcion2 src="assets/imagenes/{{opciones[1].palabra}}.png" [id]="opciones[1].letra" [class]="opciones[1].palabra">
-        <img #opcion3 src="assets/imagenes/{{opciones[2].palabra}}.png" [id]="opciones[2].letra" [class]="opciones[2].palabra">
-      </div>
+    </div>
+    <div id="imagenes-letra">
+      <img #opcion1 src="assets/imagenes/{{opciones[0].palabra}}.png" [id]="opciones[0].letra" [class]="opciones[0].palabra">
+      <img #opcion2 src="assets/imagenes/{{opciones[1].palabra}}.png" [id]="opciones[1].letra" [class]="opciones[1].palabra">
+      <img #opcion3 src="assets/imagenes/{{opciones[2].palabra}}.png" [id]="opciones[2].letra" [class]="opciones[2].palabra">
     </div>
   </ion-content>
   `
@@ -39,7 +39,13 @@ export class ElegirImagen implements AfterViewInit {
   opciones:LetraModel[] = [];
   imagenArriba:boolean = false;
   intentos:Observable<any>;
+  letraClicks:Observable<any>;
+  audioChing = new Audio('/assets/ching.mp3');
+  audioLetraAhora = new Audio;
+  audioOpciones = {};
+
   @ViewChildren('opcion1,opcion2,opcion3') opcionesBotones:QueryList<ElementRef>;
+  @ViewChild('letraAhoraRef') letraAhoraRef:ElementRef;
 
   constructor(
     public navCtrl: NavController,
@@ -49,6 +55,14 @@ export class ElegirImagen implements AfterViewInit {
   }
 
   ngAfterViewInit() {
+    // que un 'click' en la letra haga sonar el audio de la letra
+    this.audioLetraAhora.src = 'assets/letra_sonidos/' + this.letraAhora + '.MP3';
+    this.letraClicks = Observable.fromEvent(this.letraAhoraRef.nativeElement, 'click');
+    this.letraClicks
+      .throttleTime(1000)
+      .subscribe(clickEvent => {
+        this.audioLetraAhora.play();
+      });
 
     // loop through QueryList and return a single Observable of events
     this.opcionesBotones.reduce( (acc, boton) => {
@@ -67,43 +81,38 @@ export class ElegirImagen implements AfterViewInit {
 
     // act on button clicks
     this.intentos
-      // .map(clickEvent => {
-      //   console.log(clickEvent);
-      //   return clickEvent.target;
-      // })
+      .throttleTime(1000)    
       .subscribe(clickEvent => {
         // console.log(clickEvent);
 
         let letra = clickEvent.target.id;
         let palabra = clickEvent.target.classList[0];
-        this.escucharPalabra(palabra);
+        this.escuchar(palabra);
 
         if( letra === this.letraAhora ) {
           // note these setTimeout execute 
           let delay1 = 500,
               delay2 = delay1 + 1500,
               delay3 = delay2 + 1500,
-              delay4 = delay3 + 1500,
-              delay5 = delay4 + 1500;
-            
-          this.celebrar(clickEvent.target);
+              delay4 = delay3 + 1500;
 
           setTimeout(
             function(){
+              this.celebrar(clickEvent.target);
               this.subirImagen();
             }.bind(this),
             delay1
           );
           setTimeout(
             function(){
-              this.escucharLetra(letra);
+              this.escuchar(letra);
             }.bind(this),
             delay2
           );
           setTimeout(
             function(){
               this.mostrarPalabra(palabra);
-              this.escucharPalabra(palabra);
+              this.escuchar(palabra);
             }.bind(this),
             delay3
           );
@@ -119,11 +128,9 @@ export class ElegirImagen implements AfterViewInit {
     ;
   }
   
-  //mousedown visual indication for buttons?
-
   celebrar(boton){
     boton.classList.add('woohoo');
-    console.log('algo visual, escuchar ching');
+    this.audioChing.play();    
   }
 
   subirImagen(){
@@ -131,12 +138,8 @@ export class ElegirImagen implements AfterViewInit {
     // animacion sube el imagen, achica la letra
   }
 
-  escucharLetra(letra:string) {
-    console.log('escuchar audio Letra ' + letra);
-  }
-
-  escucharPalabra(palabra:string) {
-    console.log('escuchar audio palabra ' + palabra);
+  escuchar(letraOPalabra:string) {
+    this.audioOpciones[letraOPalabra].play();    
   }
 
   mostrarPalabra (palabra:string) {
@@ -154,13 +157,26 @@ export class ElegirImagen implements AfterViewInit {
 
   nuevaLetra(alfabeto){
     this.alfabetoAlAzar = this.shuffle(alfabeto);
-    this.letraAhora = this.alfabetoAlAzar[0].letra;
 
+    // letra principal
+    this.letraAhora = this.alfabetoAlAzar[0].letra;
+    this.audioLetraAhora.src = 'assets/letra_sonidos/' + this.letraAhora + '.MP3';    
+
+    // tres opciones
     this.opciones = this.shuffle([
       this.alfabetoAlAzar[0], 
       this.alfabetoAlAzar[1], 
       this.alfabetoAlAzar[2]
     ]);
+
+    // objeto de sonidos para todos las opciones
+    this.opciones.forEach(Letra  => {
+      this.audioOpciones[Letra.letra] = new Audio('/assets/letra_sonidos/' + Letra.letra + '.MP3');
+      // archivos palabra en puro menisculo
+      var palabraLower = Letra.palabra.toLowerCase();      
+      this.audioOpciones[Letra.palabra] = new Audio('/assets/palabras/' + palabraLower + '.MP3');
+    });
+
   }
 
   shuffle(array)
