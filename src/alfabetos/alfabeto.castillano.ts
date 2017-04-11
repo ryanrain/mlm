d6eebfd61606b5641696f6b553a659c7428031c9
Ryan Wadsworth <ryanrain@gmail.com>
2017-04-10 21:38:58 -0500

    update spanish letters and words

--- src/alfabetos/alfabeto.castillano.ts
+++ src/alfabetos/alfabeto.castillano.ts
@@ -2,71 +2,111 @@ import { LetraModel } from '../models/letra.model';
 
 export class AlfabetoCastillano {
 
-    alfabeto: LetraModel[] = [];
-    // alfabetoCastillano = [];
-
-    constructor() {
-        let alfabetoCastillano:LetraModel[] = [
-            {
-                letra: 'A',
-                palabra: 'Aguacate'
-            },
-            {
-                letra: 'B',
-                palabra: 'Banano'
-            },
-            {
-                letra: 'C',
-                palabra: 'Casa'
-            },
-            {
-                letra: 'D',
-                palabra: 'Djlklkj'
-            },
-            {
-                letra: 'E',
-                palabra: 'Elefante'
-            },
-            {
-                letra: 'F',
-                palabra: 'Fuego'
-            },
-            {
-                letra: 'G',
-                palabra: 'Guayaba'
-            },
-            {
-                letra: 'H',
-                palabra: 'Helado'
-            },
-            {
-                letra: 'I',
-                palabra: 'Iguana'
-            },
-            {
-                letra: 'J',
-                palabra: 'Jlkj'
-            },
-            {
-                letra: 'K',
-                palabra: 'Klklkj'
-            },
-            {
-                letra: 'L',
-                palabra: 'Llkjlkj'
-            },
-            {
-                letra: 'M',
-                palabra: 'Mango'
-            },
-            {
-                letra: 'N',
-                palabra: 'Nljklkj'
-            }        
-        ];
-        for(let letra of alfabetoCastillano) {
-            this.alfabeto.push( letra );
-        }
-    }
+    alfabeto: LetraModel[] = [
+        {
+            letra: 'a',
+            palabra: 'aguacate'
+        },
+        {
+            letra: 'b',
+            palabra: 'burro'
+        },
+        {
+            letra: 'c',
+            palabra: 'cama'
+        },
+        {
+            letra: 'd',
+            palabra: 'dedo'
+        },
+        {
+            letra: 'e',
+            palabra: 'elefante'
+        },
+        {
+            letra: 'f',
+            palabra: 'foco'
+        },
+        {
+            letra: 'g',
+            palabra: 'gallo'
+        },
+        {
+            letra: 'h',
+            palabra: 'huevo'
+        },
+        {
+            letra: 'i',
+            palabra: 'iguana'
+        },
+        {
+            letra: 'j',
+            palabra: 'jarra'
+        },
+        {
+            letra: 'k',
+            palabra: 'koala'
+        },
+        {
+            letra: 'l',
+            palabra: 'luna'
+        },
+        {
+            letra: 'll',
+            palabra: 'llama'
+        },
+        {
+            letra: 'm',
+            palabra: 'mano'
+        },
+        {
+            letra: 'n',
+            palabra: 'niña'
+        },
+        {
+            letra: 'o',
+            palabra: 'oso'
+        },
+        {
+            letra: 'p',
+            palabra: 'piñata'
+        },
+        {
+            letra: 'q',
+            palabra: 'queso'
+        },
+        {
+            letra: 'r',
+            palabra: 'rosa'
+        },
+        {
+            letra: 's',
+            palabra: 'silla'
+        },
+        {
+            letra: 't',
+            palabra: 'tortuga'
+        },
+        {
+            letra: 'u',
+            palabra: 'uva'
+        },
+        {
+            letra: 'v',
+            palabra: 'vaca'
+        },
+        {
+            letra: 'x',
+            palabra: 'xochitl'
+        },
+        {
+            letra: 'y',
+            palabra: 'yoyo'
+        },
+        {
+            letra: 'z',
+            palabra: 'zapata'
+        }  
+    ];
 }
-// alfabeto = ['a','b','c','ch','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'];
+// alfabeto = ['a','b','c','ch','d','e','f','g','h','i','j','k','l','ll','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'];
