ff2f016290992f62808e2793a8c1207c21b0edc7
Ryan Wadsworth <ryanrain@gmail.com>
2017-04-30 06:53:02 -0500

    lines accomplished with svg, cyclability

--- src/pages/matching/matching.ts
+++ src/pages/matching/matching.ts
@@ -1,11 +1,11 @@
-import { Component, ViewChildren, AfterViewInit, QueryList, ElementRef } from '@angular/core';
+import { Component, ViewChild, ViewChildren, AfterViewInit, QueryList, ElementRef, HostListener } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
 // import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/merge';
 import 'rxjs/add/operator/throttleTime';
 
-import { NavController } from 'ionic-angular';
+import { NavController, Content } from 'ionic-angular';
 
 import { PalabrasCastillano } from '../../alfabetos/palabras.castillano';
 
@@ -13,18 +13,18 @@ import { PalabrasCastillano } from '../../alfabetos/palabras.castillano';
   selector: 'matching-game',
   template: `
   <button class="nav-button" (click)="volver()"><ion-icon name="home"></ion-icon></button>
-  <button class="nav-button refresh" (click)="createWords()"><ion-icon name="refresh"></ion-icon></button>
+  <button class="nav-button refresh" (click)="reset()"><ion-icon name="refresh"></ion-icon></button>
 
   <ion-content padding>
+    <svg #lines id="lines" viewBox="0 0 320 600"></svg>
     <div id="words">
-      <div *ngFor="let word of wordsText"><span #words [attr.data-word]="word">{{word}}</span></div>
+      <div *ngFor="let word of wordsText"><span #words [attr.data-word]="word" [class]="word">{{word}}</span></div>
     </div>
     <div id="images">
       <div *ngFor="let word of words">
-        <img #images src="assets/imagenes/{{word}}.png" [attr.data-word]="word">
+        <img #images [class]="word" src="assets/imagenes/{{word}}.png" [attr.data-word]="word">
       </div>      
     </div>
-    <canvas id="lines"></canvas>
   </ion-content>
   <div style="display:none;" class="preloader">
   </div>
@@ -36,23 +36,39 @@ export class Matching implements AfterViewInit {
   wordsText:String[];
   wordAttempted:String = '';
   imgOrText:String = '';
+  numberCorrectSoFar:Number = 0;
+  notYetRun:boolean;
 
   audioChing = new Audio('/assets/ching.mp3');
 
   @ViewChildren('words, images') clickables:QueryList<ElementRef>;
+  @ViewChild('lines') lines:ElementRef;
   attempts:Observable<any>;
+  @ViewChild(Content) content: Content;
+
 
   constructor(
     public navCtrl: NavController,
-    public castillano: PalabrasCastillano
+    public castillano: PalabrasCastillano,
       ) {
       this.createWords();
   }
 
   ngAfterViewInit() {
+
+    this.setSvgViewbox();
+
+    this.clickables.changes.subscribe(buttons => {
+      this.makeButtonsClickable(buttons);
+    })
+
+    this.makeButtonsClickable(this.clickables); // first run
+  }
+
+  makeButtonsClickable(buttons:QueryList<ElementRef>) {
     
     // loop through QueryList and return a single Observable of events
-    this.clickables.reduce( (acc, boton) => {
+    buttons.reduce( (acc, boton) => {
         let current = Observable.fromEvent(boton.nativeElement, 'click');
         if ( acc === null) {
           // console.log('first run');
@@ -98,11 +114,19 @@ export class Matching implements AfterViewInit {
           setTimeout(function() {    
             if ( this.wordAttempted === currentAttempt ) {
               console.log('if the next one is correct');
+              this.numberCorrectSoFar++;
               this.audioChing.play();
-              // draw a straight line 
-              
-              // make those no longer clickable
+              this.drawLine(clickEvent.target);              
               this.deselectAll();
+
+
+              // make those no longer clickable
+
+              if(this.numberCorrectSoFar === 5) {
+                setTimeout(()=>{
+                  this.celebrate();
+                },2000);
+              }
             } else {
               console.log('incorrect');
               this.deselectAll();
@@ -110,10 +134,54 @@ export class Matching implements AfterViewInit {
           }.bind(this), 250);
         }
 
-
-
       }
     );
+  }
+
+  setSvgViewbox() {
+    let width = this.content.getContentDimensions().contentWidth;
+    let height = this.content.getNativeElement().offsetHeight;
+    this.lines.nativeElement.setAttribute("viewBox", '0 0 ' + width + ' ' + height);
+  }
+
+  drawLine(target:any){
+
+    let leftOffsetOfMostRecent, topOffsetOfMostRecent, leftOffsetOfOtherSelected, topOffsetOfOtherSelected;
+
+    // find the other selected item
+    let otherSelected = this.clickables.filter(item => {
+      if ( item.nativeElement.classList.contains('selected') && item.nativeElement.nodeName !== target.nodeName) {
+        return true;
+      }
+    })[0].nativeElement;
+
+    if (target.nodeName === 'SPAN') {
+      leftOffsetOfMostRecent = target.offsetWidth + 8; // .5em padding
+      leftOffsetOfOtherSelected = otherSelected.offsetParent.offsetLeft + otherSelected.offsetParent.offsetWidth - otherSelected.offsetWidth;
+    } else {
+      leftOffsetOfMostRecent = target.offsetParent.offsetLeft + target.offsetParent.offsetWidth - target.offsetWidth;
+      leftOffsetOfOtherSelected = otherSelected.offsetWidth + 8; // .5em padding
+    }
+    topOffsetOfMostRecent = target.offsetTop + (target.offsetHeight / 2)
+    topOffsetOfOtherSelected = otherSelected.offsetTop + (otherSelected.offsetHeight / 2)
+    
+
+    console.log(otherSelected);
+    otherSelected.nativeElement
+
+    let newpath = document.createElementNS('http://www.w3.org/2000/svg',"path"); 
+    newpath.setAttribute('d', 'M ' + leftOffsetOfMostRecent + ',' + topOffsetOfMostRecent + ',L' + leftOffsetOfOtherSelected + ',' + topOffsetOfOtherSelected);
+
+    this.lines.nativeElement.appendChild(newpath);
+
+  }
+
+  findOtherSelected(list:QueryList<ElementRef>, targetNodeName:string) {
+    return list.filter(item => {
+      if ( item.nativeElement.classList.contains('selected') && item.nativeElement.nodeName !== targetNodeName) {
+        return true;
+      }
+    });
   }
 
   deselectAll() {
@@ -124,31 +192,22 @@ export class Matching implements AfterViewInit {
     });
   }
 
-  drawLine(){
-    /**
-     * first assemble a nodelist .filter with hasClass('selected')
-     * create a div
-     * set it's   
-     */
-      // var canvas = document.getElementById('myCanvas');
-      // var context = canvas.getContext('2d');
-
-      // context.beginPath();
-      // context.moveTo(100, 150);
-      // context.lineTo(450, 50);
-      // context.lineWidth = 10;
-
-      // // set line color
-      // context.strokeStyle = '#ff0000';
-      // context.stroke();
-  }
-
   createWords(){
     this.words = this.shuffle(this.castillano.words).slice(0,5);
     this.wordsText = this.shuffle(this.words.slice());
   }
 
+  celebrate() {
+    this.audioChing.play();
+    this.reset();
+  }
+
   reset() {
+    // erase all lines
+    while (this.lines.nativeElement.firstChild) {
+        this.lines.nativeElement.removeChild(this.lines.nativeElement.firstChild);
+    }
+    this.createWords();
   }
 
   shuffle(array)
@@ -168,4 +227,19 @@ export class Matching implements AfterViewInit {
     this.navCtrl.pop();
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(){
+
+    console.log(this.notYetRun);
+    // debounced resize
+    if ( this.notYetRun === true || typeof(this.notYetRun) === 'undefined') {
+      this.reset();
+      this.setSvgViewbox();
+      this.notYetRun = false;
+      setTimeout(()=>{
+        this.notYetRun = true;
+      }, 1000); 
+    }
+
+  }
 }
