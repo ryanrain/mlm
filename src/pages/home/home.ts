33f4e75b924a1a4fb56d98fffaa8479011473bbc
Ryan Wadsworth <ryanrain@gmail.com>
2017-05-11 20:28:26 -0500

    menu styles, clickable maguito

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,22 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Observable} from 'rxjs/Observable';
 
 import { ElegirImagen } from '../elegirImagen/elegirImagen';
 import { Bloques } from '../bloques/bloques';
+import { Matching } from '../matching/matching';
+import { Lectura } from '../lectura/lectura';
+
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  // templateUrl: 'home.html'
+  template: `
+    <ion-content padding>
+      <div id="mlm"></div>
+      <div id="menu-buttons"><div *ngFor="let p of pages" (click)="openPage(p)">{{p.title}}</div></div>
+      <div #maguito id="maguito"></div>
+    </ion-content>
+  `
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
   
   pages: any[] = [
-    { title: 'Elegir Imagen', component: ElegirImagen },
-    { title: 'Bloques', component: Bloques }
+    { title: 'Letras', component: ElegirImagen },
+    { title: 'Bloques', component: Bloques },
+    { title: 'Pares', component: Matching },
+    { title: 'Lecturas de Comprensión', component: Lectura }
   ]
+  @ViewChild('maguito') maguito:ElementRef;
+  maguitoClicks:Observable<any>;
+  audioChing = new Audio('/assets/ching.mp3');
 
   constructor(public navCtrl: NavController) {
-    
+  }
+
+  ngAfterViewInit() {
+    this.maguitoClicks = Observable.fromEvent(this.maguito.nativeElement, 'click');
+    this.maguitoClicks.subscribe(event => {
+      this.audioChing.play();
+    });
   }
 
   openPage(page) {
